3361a569daa936ce4701cc64bdbb47f25b2f66a8
fernandacastro <fcastro.rj@gmail.com>
2023-10-21 15:31:54 +0200

    Refactoring module event: common-config-data

--- purchase-service/src/main/java/com/fcastro/purchaseService/PurchaseServiceApplication.java
+++ purchase-service/src/main/java/com/fcastro/purchaseService/PurchaseServiceApplication.java
@@ -2,8 +2,10 @@ package com.fcastro.purchaseService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.fcastro"})
 public class PurchaseServiceApplication {
 
     public static void main(String[] args) {
