f3cf479f84332fa9085d0c160798ba1edccb2016
fernandacastro <fcastro.rj@gmail.com>
2023-11-07 00:05:24 +0100

    Post Pantry Consume

--- pantry-web/src/Components/Consume.js
+++ pantry-web/src/Components/Consume.js
@@ -7,6 +7,7 @@ export default function Consume() {
   const pantry = useContext(PantryContext);
   const [pantryItems, setPantryItems] = useState([]);
   const [consumedItems, setConsumedItems] = useState([]);
+  const [hasConsume, setHasConsume] = useState(false);
 
   useEffect(() => {
 
@@ -21,7 +22,7 @@ export default function Consume() {
 
 
   function loadConsumedItemsList(data) {
-    let copy = [...consumedItems];
+    let copy = [];
 
     data.forEach((item) => {
       copy = [...copy,
@@ -32,6 +33,7 @@ export default function Consume() {
       }
       ]
     })
+    setHasConsume(false)
     return setConsumedItems(copy);
   }
 
@@ -55,8 +57,15 @@ export default function Consume() {
   }
 
   function handleIncrease(index, qty) {
+    let notZero = false;
+
     const array = consumedItems.map((c, i) => {
+      if (c.qty > 0) {
+        notZero = true;
+      }
+
       if (i === index) {
+
         return c =
         {
           ...c,
@@ -66,11 +75,28 @@ export default function Consume() {
         return c;
       }
     });
+    setHasConsume(notZero);
     return setConsumedItems(array);
   }
 
   function handleSave() {
-    //post("/pantry/" + pantry.id + "/consume", consumedItems);
+    fetch('http://localhost:8080/pantry/' + pantry.id + '/consume',
+      {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(consumedItems)
+      })
+
+      .then((response) => response.json())
+      .then((response) => {
+        setPantryItems(response);
+        loadConsumedItemsList(response)
+      })
+      .catch((error) => { console.log(error) })
+
+    alert("Pantry updated successfully!");
   }
 
   function renderItem(index, item) {
@@ -79,11 +105,16 @@ export default function Consume() {
 
     return (
       <li className="collection-item avatar" key={item.productId}>
-        <img src="../healthy-food.png" alt="" className="circle" />
-        <span className="title">{item.product.code}</span>
+        <img src="./healthy-food.png" alt="" className="circle" />
+
+        <div className="col s4"><span className="title">{item.product.code}</span></div>
+        <div className="col s2"><span className="teal-text text-lighten-2" >Current</span></div>
+        <div className="col s2"><span className="teal-text text-lighten-2" >Provisioned</span></div>
+        <div className="col s2"><span className="teal-text text-lighten-2" >Last Provisionig</span></div>
+
         <div className="row">
 
-          <div className="col s5">
+          <div className="col s4">
             <p>
               {item.product.description} <br />
               {item.product.size}
@@ -95,19 +126,25 @@ export default function Consume() {
           </div>
 
           <div className="col s2">
+            <span>{item.provisionedQty}</span>
+          </div>
+
+          <div className="col s2">
+            <span>{item.lastProvisioning}</span>
+          </div>
+
+          <div className="col s2">
             <center>
-              <a className="left waves-effect waves-circle waves-light btn-floating secondary-content-content"
-                onClick={() => handleDecrease(index, consumedItem.qty)}>
+              <a href="#!" className="left waves-effect waves-circle waves-light btn-floating secondary-content-content"
+                style={consumedItem.qty > 0 ? { pointerEvents: "auto" } : { pointerEvents: "none" }}
+                onClick={() => handleDecrease(index, consumedItem.qty)} >
                 <i className="material-icons" >remove</i></a>
-              <input type="text" value={consumedItem.qty} />
-              <a className="right waves-effect waves-circle waves-light btn-floating secondary-content-content"
+              <span>{consumedItem.qty}</span>
+              <a href="#!" className="right waves-effect waves-circle waves-light btn-floating secondary-content-content"
+                style={consumedItem.qty > 0 ? { pointerEvents: "auto" } : { pointerEvents: "none" }}
                 onClick={() => handleIncrease(index, consumedItem.qty)}>
                 <i className="material-icons" >add</i></a>
             </center>
-          </div>
-
-          <div className="col s3">
-            <h6>Any other button</h6>
           </div>
         </div>
       </li>
@@ -128,7 +165,23 @@ export default function Consume() {
             <li className="collection-header teal-text text-lighten-2"><h5>Pantry {pantry.name}</h5></li>
             {renderItems()}
           </ul>
-          <button onClick={handleSave}>Save</button>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col 3 offset-s9'>
+          <a href="#!" className='waves-effect waves-light btn-small'
+            style={hasConsume ? { pointerEvents: "auto" } : { pointerEvents: "none" }}
+            onClick={() => loadConsumedItemsList(pantryItems)}>
+            <i className="material-icons left">clear</i>
+            Clear
+          </a>
+          &nbsp;&nbsp;
+          <a href="#!" className="right waves-effect waves-light btn-small"
+            style={hasConsume ? { pointerEvents: "auto" } : { pointerEvents: "none" }}
+            onClick={handleSave}>
+            <i className="material-icons left">done_all</i>
+            Save
+          </a>
         </div>
       </div>
     </>
