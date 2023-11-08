72925822fe00c1deb641a06380724696d4c2cf52
fernandacastro <fcastro.rj@gmail.com>
2023-11-08 10:33:03 +0100

    refactoring anf Bugfixes of buttons

---
+++ purchase-service/src/main/java/com/fcastro/purchase/exception/PurchaseItemsMissingException.java
@@ -0,0 +1,11 @@
+package com.fcastro.purchase.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class PurchaseAlreadyProcessedException extends RuntimeException {
+    public PurchaseAlreadyProcessedException(String message) {
+        super(message);
+    }
+}
