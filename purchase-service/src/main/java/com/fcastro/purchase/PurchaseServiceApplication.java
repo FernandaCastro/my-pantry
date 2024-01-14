792805fc376f9fd9ad2839b6c899c32692c1fc97
fernandacastro <fcastro.rj@gmail.com>
2024-01-14 22:16:17 +0100

    Add com.fcastro.security to scanBasePackages

--- purchase-service/src/main/java/com/fcastro/purchase/PurchaseServiceApplication.java
+++ purchase-service/src/main/java/com/fcastro/purchase/PurchaseServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
-        scanBasePackages = {"com.fcastro.kafka", "com.fcastro.purchase"}
+        scanBasePackages = {"com.fcastro.kafka", "com.fcastro.security", "com.fcastro.purchase"}
 )
 public class PurchaseServiceApplication {
 
