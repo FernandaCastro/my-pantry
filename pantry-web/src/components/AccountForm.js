05d8fb06f361b26a33c73435fdc20fefd397cf4e
fernandacastro <fcastro.rj@gmail.com>
2024-04-01 16:55:22 +0200

    new feature: pre-register an account

---
+++ pantry-web/src/components/AccountForm.js
@@ -0,0 +1,68 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import Row from 'react-bootstrap/Row';
+import VariantType from './VariantType.js';
+import { AlertContext } from '../services/context/AppContext.js';
+import { useState, useContext } from 'react';
+import { createAccount } from '../services/apis/mypantry/requests/AccountRequests';
+
+export default function AccountForm({ handleSaveSuccess }) {
+
+    const { setAlert } = useContext(AlertContext);
+
+    async function fetchCreateAccount(body) {
+        try {
+            const res = await createAccount(body);
+            showAlert(VariantType.SUCCESS, "Account added successfully.");
+            handleSaveSuccess(res);
+
+        } catch (error) {
+            showAlert(VariantType.DANGER, error.message);
+        }
+    }
+
+    function handleSubmit(e) {
+        // Prevent the browser from reloading the page
+        e.preventDefault();
+
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+
+        let formJson = Object.fromEntries(formData.entries());
+
+        fetchCreateAccount(formJson);
+
+        console.log(formJson);
+    }
+
+    function showAlert(type, message) {
+        setAlert({
+            show: true,
+            type: type,
+            message: message
+        })
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                <Form.Group className="mb-2 w-50" >
+                    <Form.Label size="sm" className="mb-1 title">Name</Form.Label>
+                    <Form.Control size="sm" type="text" name="name" />
+                </Form.Group>
+                <Form.Group className="mb-2 w-50" >
+                    <Form.Label size="sm" className="mb-1 title">Email</Form.Label>
+                    <Form.Control size="sm" type="text" name="email" />
+                </Form.Group>
+            </Row>
+            <Row>
+                <Stack direction="horizontal" gap={2} className="mb-3 mt-3 d-flex justify-content-end">
+                    <Button bsPrefix='btn-custom' type="reset" size="sm">Clear</Button>
+                    <Button bsPrefix='btn-custom' type="submit" size="sm">Save</Button>
+                </Stack>
+            </Row>
+        </Form>
+    )
+}
