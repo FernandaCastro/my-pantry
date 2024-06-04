d6aff5048e2cccccc1fb9f66a8a10113df01453f
fernandacastro <fcastro.rj@gmail.com>
2024-06-04 21:25:35 +0200

    feature-v0.2.0: purchase-service: scan LocaleConfig

--- purchase-service/src/main/java/com/fcastro/purchaseservice/PurchaseServiceApplication.java
+++ purchase-service/src/main/java/com/fcastro/purchaseservice/PurchaseServiceApplication.java
@@ -2,14 +2,12 @@ package com.fcastro.purchaseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
-        scanBasePackages = {"com.fcastro.kafka", "com.fcastro.security", "com.fcastro.purchaseservice"}
+        scanBasePackages = {"com.fcastro.app.config", "com.fcastro.kafka", "com.fcastro.security", "com.fcastro.purchaseservice"}
 )
-@EnableJpaRepositories({"com.fcastro.security", "com.fcastro.purchaseservice"})
-@EntityScan({"com.fcastro.security", "com.fcastro.purchaseservice"})
+//@EnableJpaRepositories({"com.fcastro.security", "com.fcastro.purchaseservice"})
+//@EntityScan({"com.fcastro.security", "com.fcastro.purchaseservice"})
 public class PurchaseServiceApplication {
 
     public static void main(String[] args) {
