2e4bec8d436b6f87248a774df0179a117aa225fc
fernandacastro <fcastro.rj@gmail.com>
2024-06-21 20:09:10 +0200

    add accountservice context

--- account-service/src/main/java/com/fcastro/accountservice/accesscontrol/AccessControlController.java
+++ account-service/src/main/java/com/fcastro/accountservice/accesscontrol/AccessControlController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/accessControl")
+@RequestMapping("/accountservice/accessControl")
 public class AccessControlController {
 
     private final AccessControlService accessControlService;
