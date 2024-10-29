494869fb8ce411e060f1f549b00bb9045f92bf57
fernandacastro <fcastro.rj@gmail.com>
2024-10-29 09:48:20 +0100

    Explicitly acknowledgement of received and processed events

--- purchase-service/src/main/java/com/fcastro/purchaseservice/config/ProductEventListener.java
+++ purchase-service/src/main/java/com/fcastro/purchaseservice/config/ProductEventListener.java
@@ -7,6 +7,7 @@ import com.fcastro.purchaseservice.product.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,7 +25,7 @@ public class ProductEventListener {
     }
 
     @KafkaListener(topics = "productTopic", containerFactory = "kafkaListenerContainerFactory")
-    protected void listener(ProductEvent event) {
+    protected void listener(ProductEvent event, Acknowledgment acknowledgment) {
         LOG.info("Event Received: Topic[{}], Data[{}]", kafkaConfigData.getProductTopic(), event.toString());
 
         try {
@@ -32,9 +33,12 @@ public class ProductEventListener {
                 throw new EventProcessingException("ProductEvent received, but attribute data is null.");
             }
             productService.processProductEvent(event.getData());
+
         } catch (EventProcessingException ex) {
             //TODO: What should be done? Save to reprocess later or manually?
             LOG.error(ex.getMessage());
+        } finally {
+            acknowledgment.acknowledge();
         }
     }
 }
--- purchase-service/src/main/java/com/fcastro/purchaseservice/config/PurchaseEventListener.java
+++ purchase-service/src/main/java/com/fcastro/purchaseservice/config/PurchaseEventListener.java
@@ -7,6 +7,7 @@ import com.fcastro.purchaseservice.purchaseItem.PurchaseItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,18 +26,23 @@ public class PurchaseEventListener {
 
     //TODO: HOW TO define <topics> getting value from configuration files?
     @KafkaListener(topics = "purchaseCreateTopic", containerFactory = "kafkaListenerContainerFactory")
-    protected void listener(PurchaseEvent event) {
+    protected void listener(PurchaseEvent event, Acknowledgment acknowledgment) {
 
-        if (event.getData() == null) {
-            LOGGER.error("PurchaseCreateEvent received, but attribute data is null.");
-            throw new EventProcessingException("PurchaseCreateEvent received, but attribute data is null.");
+        try {
+            if (event.getData() == null) {
+                LOGGER.error("PurchaseCreateEvent received, but attribute data is null.");
+                throw new EventProcessingException("PurchaseCreateEvent received, but attribute data is null.");
+            }
+
+            LOGGER.info("Event Received: Topic[{}], Data[{}]", kafkaConfigData.getPurchaseCreateTopic(), event.toString());
+
+            purchaseItemService.processPurchaseEvent(event.getData());
+
+        } catch (EventProcessingException ex) {
+            //TODO: What should be done? Save to reprocess later or manually?
+            LOGGER.error(ex.getMessage());
+        } finally {
+            acknowledgment.acknowledge();
         }
-
-        LOGGER.info("Event Received: Topic[{}], Data[{}]",
-                kafkaConfigData.getPurchaseCreateTopic(),
-                event.toString()
-        );
-
-        purchaseItemService.processPurchaseEvent(event.getData());
     }
 }
