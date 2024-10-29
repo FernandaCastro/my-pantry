fb27b3b10e5d3cdecce5a2bafd6265486408fb77
fernandacastro <fcastro.rj@gmail.com>
2024-10-29 09:56:04 +0100

    Kafka Listeners: ack-mode = MANUAL-IMMEDIATE Kafka Producers: acks = 1 (only by leader)

--- common-config-data/src/main/java/com/fcastro/kafka/config/KafkaEventConfig.java
+++ common-config-data/src/main/java/com/fcastro/kafka/config/KafkaEventConfig.java
@@ -2,6 +2,7 @@ package com.fcastro.kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -12,6 +13,7 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -24,19 +26,31 @@ public class KafkaEventConfig<V extends Serializable> {
 
     private final KafkaConfigData kafkaConfigData;
 
-    private HashMap<String, Object> factoryProperties;
+    private HashMap<String, Object> producerFactoryProperties;
+    private HashMap<String, Object> consumerFactoryProperties;
 
     public KafkaEventConfig(KafkaConfigData kafkaConfigData) {
         this.kafkaConfigData = kafkaConfigData;
-        setFactoryProperties();
+        setProducerFactoryProperties();
+        setConsumerFactoryProperties();
     }
 
-    private void setFactoryProperties() {
-        factoryProperties = new HashMap<String, Object>();
-        factoryProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServersConfig());
-        factoryProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        factoryProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        factoryProperties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfigData.getGroup());
+    private void setProducerFactoryProperties() {
+        producerFactoryProperties = new HashMap<String, Object>();
+        producerFactoryProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServersConfig());
+        producerFactoryProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        producerFactoryProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        producerFactoryProperties.put(ProducerConfig.ACKS_CONFIG, "1"); //ACKS= 1 (ack only by  the leader)
+    }
+
+    private void setConsumerFactoryProperties() {
+        consumerFactoryProperties = new HashMap<String, Object>();
+        consumerFactoryProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServersConfig());
+        consumerFactoryProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerFactoryProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        consumerFactoryProperties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfigData.getGroup());
+        consumerFactoryProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        //consumerFactoryProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     }
 
     @Bean
@@ -47,13 +61,14 @@ public class KafkaEventConfig<V extends Serializable> {
         eventDeserializer.addTrustedPackages("*");
         eventDeserializer.setUseTypeMapperForKey(true);
 
-        return new DefaultKafkaConsumerFactory<>(factoryProperties, new StringDeserializer(), eventDeserializer);
+        return new DefaultKafkaConsumerFactory<>(consumerFactoryProperties, new StringDeserializer(), eventDeserializer);
     }
 
     @Bean
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, V>> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, V> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
         return factory;
     }
 
@@ -62,7 +77,7 @@ public class KafkaEventConfig<V extends Serializable> {
         var eventSerializer = new JsonSerializer();
         eventSerializer.setUseTypeMapperForKey(true);
 
-        return new DefaultKafkaProducerFactory<>(factoryProperties, new StringSerializer(), eventSerializer);
+        return new DefaultKafkaProducerFactory<>(producerFactoryProperties, new StringSerializer(), eventSerializer);
     }
 
     @Bean
