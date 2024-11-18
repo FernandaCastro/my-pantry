03cc1f865404a79055de6ef69245c632bfbe9673
fernandacastro <fcastro.rj@gmail.com>
2024-11-18 12:44:04 +0100

    change: remove unused libs

--- pantry-web/webpack.config.js
+++ pantry-web/webpack.config.js
@@ -4,23 +4,15 @@ import Quagga from 'quagga'; // ES6
 const dotEnv = require('dotenv').config();
 const Quagga = require('quagga').default; // Common JS (important: default)
 const path = require('path');
-// const webpack = require('webpack');
-// const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.ts',
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-        // new NodePolyfillPlugin(),
-        // new webpack.DefinePlugin({
-        //     'process.env': {
-        //         REACT_APP_API_URL_PANTRY: JSON.stringify(process.env.REACT_APP_API_URL_PANTRY)
-        //     }
-        // }),
     ],
     output: {
         hashFunction: require('xxhash64').hashFunction,
