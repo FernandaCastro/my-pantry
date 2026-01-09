dc6b3003ad2cbfe1caaec4a2fef95c169b88b3c6
fernandacastro <fcastro.rj@gmail.com>
2026-01-09 11:09:18 +0100

    fix: mypantry-app builds and runs correctly

---
+++ mypantry-app/src/main/java/com/fcastro/mypantry/app/MyPantryApplication.java
@@ -0,0 +1,12 @@
+package com.fcastro.mypantry.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPantryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPantryApplication.class, args);
+    }
+}
